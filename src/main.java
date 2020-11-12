4953c73f9bed09047dfde75106ecac2bbd3db3c9
nikampak <nikampak@gmail.com>
2020-11-12 10:46:01 -0500

    New comment is added

--- src/main.java
+++ src/main.java
@@ -2,5 +2,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hello Git Last Day");
         // b21 is excited!
+        // b21 is dedicated!
     }
 }
